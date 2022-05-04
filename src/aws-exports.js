88b84456c439838ab429e19bb7e2ed688e09a67b
mgivneyjg <mgivney@jahnelgroup.com>
2022-05-03 23:13:05 -0400

    ci incremental

---
+++ src/aws-exports.js
@@ -0,0 +1,13 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cloud_logic_custom": [
+        {
+            "name": "vectoapi",
+            "endpoint": "https://s63ee1ttal.execute-api.us-east-1.amazonaws.com/dev",
+            "region": "us-east-1"
+        }
+    ]
+};
+
+
+export default awsmobile;
